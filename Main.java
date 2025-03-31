d6432290d047a5538d9efc101918879322439df7
delarissss <lugovskaaarina@yandex.ru>
2025-03-31 16:49:12 +0300

    Update Main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Значимое изменение 1");
+        System.out.println("Значимое изменение 2");
     }
 }
