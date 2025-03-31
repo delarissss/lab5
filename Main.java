0673402581e19afe452fdd921b513efeeabdf323
delarissss <lugovskaaarina@yandex.ru>
2025-03-31 18:13:26 +0300

    Updated Main 1.1

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
-        System.out.println("Другое значимое изменение");
+        System.out.println("Другое значимое изменение 1");
+        System.out.println("Другое значимое изменение 2");
     }
 }
